07e7f1b9e9b7b4e8b368b47b645824035e1ada75
Iaisharai <02iaisharai@gmail.com>
2020-11-20 16:58:58 +0530

    Initial Commit

---
+++ BestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BestApp
+//
+//  Created by Iaisharai Gabriel on 20/11/20.
+//  Copyright © 2020 Iaisharai Gabriel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
